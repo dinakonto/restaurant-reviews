82ab68f703dfec6dff584ab7c9bcd5a9cafa4c32
Dina Konto <dina.konto@gmail.com>
2020-05-26 10:22:47 +1000

    style: Add responsive breakpoints

--- js/main.js
+++ js/main.js
@@ -143,22 +143,26 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const wrapDetail = document.createElement('div');
+  wrapDetail.className = 'restaurant-details';
+  li.append(wrapDetail);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  wrapDetail.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  wrapDetail.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  wrapDetail.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  wrapDetail.append(more)
 
   return li
 }
